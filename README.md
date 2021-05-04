a83360e308ce94cfe2db732fddcc8146b19d34e7
Aaron DeLory <rexiosdev@gmail.com>
2021-05-04 12:37:18 -0400

    Tizen implementation works

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # workout
 
-A new flutter plugin project.
+Run a workout session and get live health data from Wear OS and Tizen.
 
 ## Getting Started
 
-This project is a starting point for a Flutter
-[plug-in package](https://flutter.dev/developing-packages/),
-a specialized package that includes platform-specific implementation code for
-Android and/or iOS.
+### Wear OS
+TODO
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### Tizen
+
+This plugin requires Tizen 5.5+.
+
+Make the following changes to `tizen/tizen-manifest.xml`
+```
+<manifest api-version="5.5" ...>
+    <privileges>
+        <privilege>http://tizen.org/privilege/healthinfo</privilege>
+    </privileges>
+    <feature name="http://tizen.org/feature/sensor.heart_rate_monitor">true</feature>
+</manifest>
+```
 
--- lib/workout.dart
+++ lib/workout.dart
@@ -1,14 +1,56 @@
-
 import 'dart:async';
+import 'dart:io' show Platform;
 
 import 'package:flutter/services.dart';
+import 'package:health_tizen/health_tizen.dart';
 
 class Workout {
-  static const MethodChannel _channel =
-      const MethodChannel('workout');
+  static const MethodChannel _channel = const MethodChannel('workout');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  final _streamController = StreamController<WorkoutReading>.broadcast();
+
+  Stream<WorkoutReading> get stream => _streamController.stream;
+
+  Future<void> start() {
+    if (Platform.isAndroid) {
+      return _startAndroid();
+    } else {
+      // Platform.isTizen
+      return _startTizen();
+    }
   }
+
+  Future<void> _startAndroid() {
+    return Future.error(UnimplementedError());
+  }
+
+  Future<void> _startTizen() {
+    final health = HealthTizen();
+
+    health.stream.listen(
+      (event) => _streamController.add(
+        WorkoutReading(
+          event.sensor == TizenSensor.hrm
+              ? WorkoutReadingType.heartRate
+              : WorkoutReadingType.unknown,
+          event.value,
+        ),
+      ),
+    );
+
+    return health.start([TizenSensor.hrm]);
+  }
+}
+
+class WorkoutReading {
+  final timestamp = DateTime.now().millisecondsSinceEpoch;
+  final WorkoutReadingType type;
+  final double value;
+
+  WorkoutReading(this.type, this.value);
+}
+
+enum WorkoutReadingType {
+  unknown,
+  heartRate,
 }
