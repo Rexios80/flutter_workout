9f1b9fda602e44829d3cb31859844e7f212bc1f6
Rexios <rexios@rexios.dev>
2022-11-02 14:11:10 -0400

    Working on updating health services

--- android/src/main/kotlin/dev/rexios/workout/WorkoutPlugin.kt
+++ android/src/main/kotlin/dev/rexios/workout/WorkoutPlugin.kt
@@ -1,10 +1,9 @@
 package dev.rexios.workout
 
 import android.os.SystemClock
-import androidx.annotation.NonNull
 import androidx.concurrent.futures.await
 import androidx.health.services.client.ExerciseClient
-import androidx.health.services.client.ExerciseUpdateListener
+import androidx.health.services.client.ExerciseUpdateCallback
 import androidx.health.services.client.HealthServices
 import androidx.health.services.client.data.*
 import androidx.lifecycle.Lifecycle
@@ -22,13 +21,13 @@ import kotlinx.coroutines.launch
 import java.time.Instant
 
 /** WorkoutPlugin */
-class WorkoutPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+class WorkoutPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, ExerciseUpdateCallback {
     private lateinit var channel: MethodChannel
     private lateinit var lifecycleScope: CoroutineScope
 
     private lateinit var exerciseClient: ExerciseClient
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "workout")
         channel.setMethodCallHandler(this)
 
@@ -36,7 +35,7 @@ class WorkoutPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
             HealthServices.getClient(flutterPluginBinding.applicationContext).exerciseClient
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
             "getSupportedExerciseTypes" -> getSupportedExerciseTypes(result)
             "start" -> start(call.arguments as Map<String, Any>, result)
@@ -44,11 +43,12 @@ class WorkoutPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
                 stop()
                 result.success(null)
             }
+
             else -> result.notImplemented()
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
         stop()
     }
@@ -57,41 +57,30 @@ class WorkoutPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         val lifecycle: Lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(binding)
         lifecycleScope = lifecycle.coroutineScope
 
-        val listener = object : ExerciseUpdateListener {
-            override fun onExerciseUpdate(update: ExerciseUpdate) {
-                this@WorkoutPlugin.onExerciseUpdate(update)
-            }
-
-            override fun onLapSummary(lapSummary: ExerciseLapSummary) {}
-            override fun onAvailabilityChanged(dataType: DataType, availability: Availability) {}
-        }
-
-        lifecycleScope.launch {
-            exerciseClient.setUpdateListener(listener).await()
-        }
+        exerciseClient.setUpdateCallback(this)
     }
 
     override fun onDetachedFromActivityForConfigChanges() {}
     override fun onReattachedToActivityForConfigChanges(p0: ActivityPluginBinding) {}
     override fun onDetachedFromActivity() {}
 
-    private fun dataTypeToString(type: DataType): String {
+    private fun dataTypeToString(type: DataType<*, *>): String {
         return when (type) {
             DataType.HEART_RATE_BPM -> "heartRate"
-            DataType.TOTAL_CALORIES -> "calories"
-            DataType.STEPS -> "steps"
+            DataType.CALORIES_TOTAL -> "calories"
+            DataType.STEPS_TOTAL -> "steps"
             DataType.DISTANCE -> "distance"
             DataType.SPEED -> "speed"
             else -> "unknown"
         }
     }
 
-    private fun dataTypeFromString(string: String): DataType {
+    private fun dataTypeFromString(string: String): DataType<*, *> {
         return when (string) {
             "heartRate" -> DataType.HEART_RATE_BPM
-            "calories" -> DataType.TOTAL_CALORIES
-            "steps" -> DataType.STEPS
-            "distance" -> DataType.DISTANCE
+            "calories" -> DataType.CALORIES_TOTAL
+            "steps" -> DataType.STEPS_TOTAL
+            "distance" -> DataType.DISTANCE_TOTAL
             "speed" -> DataType.SPEED
             else -> throw IllegalArgumentException()
         }
@@ -99,7 +88,7 @@ class WorkoutPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
     private fun getSupportedExerciseTypes(result: Result) {
         lifecycleScope.launch {
-            val capabilities = exerciseClient.capabilities.await()
+            val capabilities = exerciseClient.getCapabilitiesAsync().await()
             result.success(capabilities.supportedExerciseTypes.map { it.id })
         }
     }
@@ -114,7 +103,7 @@ class WorkoutPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         val enableGps = arguments["enableGps"] as Boolean
 
         lifecycleScope.launch {
-            val capabilities = exerciseClient.capabilities.await()
+            val capabilities = exerciseClient.getCapabilitiesAsync().await()
             if (exerciseType !in capabilities.supportedExerciseTypes) {
                 result.error("ExerciseType $exerciseType not supported", null, null)
                 return@launch
@@ -133,35 +122,30 @@ class WorkoutPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
             val aggregateDataTypes = requestedSupportedDataTypes.intersect(
                 setOf(
                     // "Total" here refers not to the aggregation but to basal + activity.
-                    DataType.TOTAL_CALORIES,
-                    DataType.STEPS,
-                    DataType.DISTANCE
+                    DataType.CALORIES_TOTAL, DataType.STEPS, DataType.DISTANCE
                 )
             )
 
-            val config = ExerciseConfig.builder()
-                .setExerciseType(exerciseType)
-                .setDataTypes(dataTypes)
-                .setAggregateDataTypes(aggregateDataTypes)
-                .setShouldEnableGps(enableGps)
-                .build()
+            val config =
+                ExerciseConfig(
+                    exerciseType = exerciseType,
+                    dataTypes = dataTypes,
+                    isAutoPauseAndResumeEnabled = false,
+                    isGpsEnabled = enableGps,
+                )
 
-            exerciseClient
-                .startExercise(config)
-                .await()
+            exerciseClient.startExerciseAsync(config).await()
 
             // Return the unsupported data types so the developer can handle them
-            result.success(mapOf(
-                "unsupportedFeatures" to requestedUnsupportedDataTypes.map {
-                    dataTypeToString(
-                        it
-                    )
-                }
-            ))
+            result.success(mapOf("unsupportedFeatures" to requestedUnsupportedDataTypes.map {
+                dataTypeToString(
+                    it
+                )
+            }))
         }
     }
 
-    private fun onExerciseUpdate(update: ExerciseUpdate) {
+    override fun onExerciseUpdateReceived(update: ExerciseUpdate) {
         val data = mutableListOf<List<Any>>()
         val bootInstant =
             Instant.ofEpochMilli(System.currentTimeMillis() - SystemClock.elapsedRealtime())
@@ -198,6 +182,13 @@ class WorkoutPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
             channel.invokeMethod("dataReceived", it)
         }
     }
+
+    override fun onLapSummaryReceived(lapSummary: ExerciseLapSummary) {}
+    override fun onRegistered() {}
+
+    override fun onRegistrationFailed(throwable: Throwable) {}
+
+    override fun onAvailabilityChanged(dataType: DataType<*, *>, availability: Availability) {}
 
     private fun stop() {
         lifecycleScope.launch {
