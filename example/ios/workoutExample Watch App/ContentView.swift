6774f54af23241a068b455f13e117b0ff22bf641
Rexios <rexios@rexios.dev>
2024-05-17 16:01:03 -0400

    Add Apple Watch app

---
+++ example/ios/workoutExample Watch App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  workoutExample Watch App
+//
+//  Created by Rexios on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ example/ios/workoutExample Watch App/workoutExampleApp.swift
@@ -0,0 +1,26 @@
+//
+//  workoutExampleApp.swift
+//  workoutExample Watch App
+//
+//  Created by Rexios on 5/17/24.
+//
+
+import HealthKit
+import SwiftUI
+
+@main
+struct workoutExample_Watch_AppApp: App {
+//    @WKApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    init() {}
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+//class AppDelegate: NSObject, WKApplicationDelegate {
+//    func handle(_ workoutConfiguration: HKWorkoutConfiguration) {}
+//}
