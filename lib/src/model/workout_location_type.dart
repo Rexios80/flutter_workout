7265eacf299ad2906b365e51e157a688a73820d5
Rexios <rexios@rexios.dev>
2024-05-17 15:20:51 -0400

    Add more enums

---
+++ lib/src/model/workout_location_type.dart
@@ -0,0 +1,18 @@
+/// This enumerated type is used to represent the location type of a workout session.
+/// 
+/// iOS only
+enum WorkoutLocationType {
+  /// unknown
+  unknown(1),
+
+  /// indoor
+  indoor(2),
+
+  /// outdoor
+  outdoor(3);
+
+  /// The unique identifier for this location type
+  final int id;
+
+  const WorkoutLocationType(this.id);
+}
---
+++ lib/src/model/workout_swimming_location_type.dart
@@ -0,0 +1,18 @@
+/// This enumerated type is used to represent the location type of a swimming workout.
+/// 
+/// iOS only
+enum WorkoutSwimmingLocationType {
+  /// unknown
+  unknown(0),
+
+  /// pool
+  pool(1),
+
+  /// openWater
+  openWater(2);
+
+  /// The unique identifier for this location type
+  final int id;
+
+  const WorkoutSwimmingLocationType(this.id);
+}
