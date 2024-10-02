b53e4829e3ff8b1df2651179fa1ea3f34b819767
Rexios <rexios@rexios.dev>
2024-10-02 00:05:04 -0400

    Improve Apple Watch app UI

--- example/ios/workoutExample Watch App/ContentView.swift
+++ example/ios/workoutExample Watch App/ContentView.swift
@@ -8,21 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var mainEO: MainEO
+  @EnvironmentObject var mainEO: MainEO
 
-    var body: some View {
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .leading) {
         Text("Send config from phone")
+        Spacer().frame(height: 10)
         if let activityType = mainEO.activityType {
-            Text("Activity Type: \(activityType)")
+          Text("Activity Type: \(activityType.rawValue)")
+          Spacer().frame(height: 10)
         }
         if let locationType = mainEO.locationType {
-            Text("Location Type: \(locationType)")
+          Text("Location Type: \(locationType.rawValue)")
+          Spacer().frame(height: 10)
         }
         if let swimmingLocationType = mainEO.swimmingLocationType {
-            Text("Swimming Location Type: \(swimmingLocationType)")
+          Text("Swimming Location Type: \(swimmingLocationType.rawValue)")
+          Spacer().frame(height: 10)
         }
         if let lapLength = mainEO.lapLength {
-            Text("Lap Length: \(lapLength)")
+          Text("Lap Length: \(lapLength)")
         }
+      }
     }
+  }
 }
